7c3ff399cc99329ff4d0a1f32b3ea5db04de8552
Prabhu.Udagatti <Prabhu.Udagatti@valtech.com>
2022-11-11 17:17:32 +0530

    Doctor completed

--- Clinic_MVC_UI/Controllers/DoctorController.cs
+++ Clinic_MVC_UI/Controllers/DoctorController.cs
@@ -170,6 +170,7 @@ namespace Clinic_MVC_UI.Controllers
 
         public async Task<IActionResult> AddPrescription(int AppointmentId)
         {
+            #region Fecthing the appointment details
             Appointment appointment = null;
             using (HttpClient client = new HttpClient())
             {
@@ -183,11 +184,13 @@ namespace Clinic_MVC_UI.Controllers
                     }
                 }
             }
+            #endregion
             return View(appointment);
         }
         [HttpPost]
         public async Task<IActionResult> AddPrescription(Appointment appointment)
         {
+            #region Again Feching the appointment details
             Appointment appointmentNew = null;
             using (HttpClient client = new HttpClient())
             {
@@ -201,14 +204,143 @@ namespace Clinic_MVC_UI.Controllers
                     }
                 }
             }
-
-            appointmentNew.Appointment_Status = 4;
+            #endregion
+            #region Updateing the appointment by using prescription, disease and Progress 
+            appointmentNew.Appointment_Status = 3;
             appointmentNew.Prescription = appointment.Prescription;
             appointmentNew.Disease = appointment.Disease;
             appointmentNew.Progress = appointment.Progress;
             appointmentNew.Bill_Amount = appointment.Bill_Amount;
+            appointmentNew.Bill_Status = 0;
+
+            ViewBag.status = "";
+            using (HttpClient client = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(appointmentNew), Encoding.UTF8, "application/json");
+                string endPoint = _configuration["WebApiBaseUrl"] + "Appointment/UpdateAppointment";
+                using (var response = await client.PutAsync(endPoint, content))
+                {
+                    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        ViewBag.status = "Ok";
+                        ViewBag.message = "Appointment Preception Updated Successfully!";
+                    }
+                    else
+                    {
+                        ViewBag.status = "Error";
+                        ViewBag.message = "appointment wrong Entries!";
+                    }
+                }
+            }
+            #endregion
 
             return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllCompleted()
+        {
+           
+           #region Doctor can see His Appointments
+            int doctorappointId = Convert.ToInt32(TempData["ProfileID"]);
+            TempData.Keep();
+            IEnumerable<Appointment> Appointments = null;
+            using (HttpClient client = new HttpClient())
+            {
+                string endPoint = _configuration["WebApiBaseUrl"] + "Appointment/GetAppointment";
+                using (var response = await client.GetAsync(endPoint))
+                {
+                    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        var result = await response.Content.ReadAsStringAsync();
+                        Appointments = JsonConvert.DeserializeObject<IEnumerable<Appointment>>(result);
+                    }
+                }
+            }
+            List<Appointment> DoctorAppointments = new List<Appointment>();
+            foreach (var item in Appointments)
+            {
+                if (doctorappointId == item.DoctorID && item.Appointment_Status==3 && item.Bill_Amount==0)
+                {
+                    DoctorAppointments.Add(item);
+                }
+            }
+            #endregion
+            return View(DoctorAppointments);
+        }
+       
+       
+        public async Task<IActionResult> GenerateBill(int AppointmentId)
+        {
+            #region Again Feching the appointment details
+            Appointment appointmentNew = null;
+            using (HttpClient client = new HttpClient())
+            {
+                string endpoint = _configuration["WebApiBaseUrl"] + "Appointment/GetAppointmentById?AppointmentId=" + AppointmentId;
+                using (var response = await client.GetAsync(endpoint))
+                {
+                    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        var result = await response.Content.ReadAsStringAsync();
+                        appointmentNew = JsonConvert.DeserializeObject<Appointment>(result);
+                    }
+                }
+            }
+            #endregion
+            #region Update the Bill
+            appointmentNew.Bill_Amount=appointmentNew.Doctor.Charges_Per_Visit;
+
+            ViewBag.status = "";
+            using (HttpClient client = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(appointmentNew), Encoding.UTF8, "application/json");
+                string endPoint = _configuration["WebApiBaseUrl"] + "Appointment/UpdateAppointment";
+                using (var response = await client.PutAsync(endPoint, content))
+                {
+                    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        ViewBag.status = "Ok";
+                        ViewBag.message = "Appointment Bill Generated!";
+                    }
+                    else
+                    {
+                        ViewBag.status = "Error";
+                        ViewBag.message = "appointment wrong Entries!";
+                    }
+                }
+            }
+            #endregion
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> PatientHistory()
+        {
+            #region View the patient History
+            int doctorappointId = Convert.ToInt32(TempData["ProfileID"]);
+            TempData.Keep();
+            IEnumerable<Appointment> Appointments = null;
+            using (HttpClient client = new HttpClient())
+            {
+                string endPoint = _configuration["WebApiBaseUrl"] + "Appointment/GetAppointment";
+                using (var response = await client.GetAsync(endPoint))
+                {
+                    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        var result = await response.Content.ReadAsStringAsync();
+                        Appointments = JsonConvert.DeserializeObject<IEnumerable<Appointment>>(result);
+                    }
+                }
+            }
+            List<Appointment> DoctorAppointments = new List<Appointment>();
+            foreach (var item in Appointments)
+            {
+                if (doctorappointId == item.DoctorID && item.Appointment_Status==3 && item.Bill_Amount>0 )
+                {
+                    DoctorAppointments.Add(item);
+                }
+            }
+            #endregion
+            return View(DoctorAppointments);
         }
 
 
