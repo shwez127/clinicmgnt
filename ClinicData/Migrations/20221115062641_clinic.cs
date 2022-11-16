f6afb5f2b05673e489d9505d9ec1597632974279
Nawaz.Sharieff <Nawaz.Sharieff@VDC01LTC2106>
2022-11-16 10:32:07 +0530

    Feedback Added

---
+++ ClinicData/Migrations/20221115062641_clinic.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ClinicData.Migrations
+{
+    public partial class clinic : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Comment",
+                table: "pending_Feedbacks",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Rating",
+                table: "pending_Feedbacks",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "departments",
+                type: "varchar(500)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Comment",
+                table: "pending_Feedbacks");
+
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "pending_Feedbacks");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "departments",
+                type: "varchar(100)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(500)",
+                oldNullable: true);
+        }
+    }
+}
